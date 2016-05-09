e0911140211e56c5af5e18938a53394534245c24
Wu Zhi Yi <wuzhiyi51@gmail.com>
2016-05-09 15:53:44 +0800

    去重

--- py_diff.py
+++ py_diff.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 # --*-- coding: UTF-8 --*--
 
-ids=[1,2,3,3,4,2,3,4,5,7,6,1]
+ids = [1, 2, 3, 3, 4, 2, 3, 4, 5, 7, 6, 1]
 
-news_id=[]
+news_id = []
 
 for id in ids:
     if id not in news_id:
         news_id.append(id)
         
 print news_id
+
+#---------------
+
+ids = [1, 2, 3, 3, 4, 2, 3, 4, 5, 7, 6, 1]
+ids = list(set(ids))
+print ids
